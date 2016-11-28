f4f62ab0edab4a80bfcc1acbabdb5608de941c35
Gmuela <gmuela92@gmail.com>
2016-11-28 11:14:12 +0100

    eliminado dasd creado holaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+/**
+ * Created by curso mañana on 28/11/2016.
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+    
+    
+}
