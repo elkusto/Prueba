dacc83a32335b72b1be4f0d9abaee5307159bd8a
Gmuela <gmuela92@gmail.com>
2016-12-01 11:41:13 +0100

    espacios

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,7 +6,7 @@
 public class HolaMundo {
     
     public static void main(String[] args){
-        System.out.println("Hola"+ args[0] + "Paco otro Paco hora de hacer merge");
+        System.out.println("Hola"+ args[0] + "Paco       otro Paco hora de hacer merge");
         System.out.println("Hola asdf");
 
     }
